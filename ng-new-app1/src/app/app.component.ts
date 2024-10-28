d91c9107ae6395a82d32c161c833a9493403b830
Raj Yadav <rajyadav751811@gmail.com>
2024-10-28 19:07:45 +0530

    New Angular App creation

--- ng-new-app1/src/app/app.component.ts
+++ ng-new-app1/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {WelcomeComponent} from './welcome/welcome.component';
+import {GreetComponent} from './greet/greet.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WelcomeComponent],
+  imports: [RouterOutlet, WelcomeComponent, GreetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
