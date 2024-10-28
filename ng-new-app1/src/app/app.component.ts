92201437def7833692deaff390d3a52f6a0d9459
Raj Yadav <rajyadav751811@gmail.com>
2024-10-28 19:15:28 +0530

    New Angular App creation

--- ng-new-app1/src/app/app.component.ts
+++ ng-new-app1/src/app/app.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {WelcomeComponent} from './welcome/welcome.component';
 import {GreetComponent} from './greet/greet.component';
+import {UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WelcomeComponent, GreetComponent],
+  imports: [RouterOutlet, WelcomeComponent, GreetComponent, UpperCasePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ng-new-app1';
+  title = 'Raj App';
+  msg: string = 'I am from App component';
+
 }
