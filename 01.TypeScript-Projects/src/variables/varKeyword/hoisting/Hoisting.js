1065859392fffa0d17002677f282b6cfef01ba9a
RajYadav75 <rajyadav751811@gmail.com>
2025-11-03 23:58:37 +0530

    Hoisting Implemented

---
+++ 01.TypeScript-Projects/src/variables/varKeyword/hoisting/Hoisting.js
@@ -0,0 +1,6 @@
+function Hoisting() {
+    x = 10;
+    console.log("X is " + x);
+    var x; // This is Hoisting
+}
+Hoisting();
---
+++ 01.TypeScript-Projects/src/variables/varKeyword/hoisting/Hoisting.ts
@@ -0,0 +1,7 @@
+function Hoisting(){
+    x = 10;
+    console.log("X is "+x);
+    var x;  // This is Hoisting
+
+}
+Hoisting();
