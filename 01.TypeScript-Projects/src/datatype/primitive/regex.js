8342c81e6b7cc83bfc45fb12550a2507887cafa3
RajYadav75 <rajyadav751811@gmail.com>
2025-11-04 23:20:14 +0530

    Regex implemented

---
+++ 01.TypeScript-Projects/src/datatype/primitive/regex.js
@@ -0,0 +1,9 @@
+var mobile = "+(44)(30) 2242 4563";
+if (mobile.match(/\+\(44\)\([0-9]{2}\)\s[0-9]{4}\s[0-9]{4}/)) {
+    console.log("OTP Sent");
+    document.write("OTP Sent");
+}
+else {
+    console.log("Invalid Mobile");
+    document.write("Invalid Mobile");
+}
