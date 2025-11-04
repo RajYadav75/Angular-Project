d1d69132dec4d8efbfe356ec6bb7693dac9de45d
RajYadav75 <rajyadav751811@gmail.com>
2025-11-04 23:11:12 +0530

    Number Data Type implemented

---
+++ 01.TypeScript-Projects/src/datatype/primitive/number.js
@@ -0,0 +1,6 @@
+var price = 3400.56;
+var rate = 2e3; //[2000]
+var bit = 10; //[10]
+document.write("price : ".concat(price, " <br> rate: ").concat(rate, " <br> number: ").concat(bit, " <br>"));
+console.log("Bit=" + bit + "\n" + "Exponent=" + rate);
+document.write("Max number : ".concat(Number.MAX_SAFE_INTEGER, " <br> Min number : ").concat(Number.MIN_SAFE_INTEGER));
---
+++ 01.TypeScript-Projects/src/datatype/primitive/number.ts
@@ -0,0 +1,7 @@
+var price:number = 3400.56;
+var rate:number = 2e3; //[2000]
+var bit:number = 0b1010; //[10]
+document.write(`price : ${price} <br> rate: ${rate} <br> number: ${bit} <br>`);
+console.log("Bit=" + bit + "\n" + "Exponent=" + rate);
+
+document.write(`Max number : ${Number.MAX_SAFE_INTEGER} <br> Min number : ${Number.MIN_SAFE_INTEGER}`)
