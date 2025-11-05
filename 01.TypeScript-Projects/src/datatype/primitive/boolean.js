59f80eeb896dd6dd364af655508da0b89c4c4906
RajYadav75 <rajyadav751811@gmail.com>
2025-11-05 09:25:12 +0530

    Boolean Implemented

---
+++ 01.TypeScript-Projects/src/datatype/primitive/boolean.js
@@ -0,0 +1,12 @@
+// var stock: boolean = true;
+// // if (stock == 1)     // invalid in typescript but valid in javascript
+// // {
+// // }
+// if (stock == true) {
+// }
+var name = prompt("Enter Name in number or String");
+var username = prompt("Enter Name");
+var username1 = prompt("Enter Name");
+document.write(username);
+document.writeln(username1);
+document.writeln(name);
---
+++ 01.TypeScript-Projects/src/datatype/primitive/boolean.ts
@@ -0,0 +1,18 @@
+// var stock: boolean = true;
+// // if (stock == 1)     // invalid in typescript but valid in javascript
+// // {
+    
+// // }
+// if (stock == true) {
+
+// }
+
+
+ var name:string|number=prompt("Enter Name in number or String");
+
+var username:string|null = prompt("Enter Name");
+var username1:string = prompt("Enter Name");   
+
+document.write(username);
+document.writeln(username1);
+document.writeln(name);
